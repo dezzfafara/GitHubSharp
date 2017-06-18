d8340b7173b3271e59eeb5a5f167c0f9b6974c3c
dezzfafara <dezzfafara@gmail.com>
2017-06-18 14:35:28 +0300

    Start

---
+++ L5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryOp b = new BinaryOp(SimpleMath.Substract);
+            Console.WriteLine(b(5, 6));
+        }
+    }
+}
---
+++ L5/SimpleMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    public delegate int BinaryOp(int x, int y);
+    class SimpleMath
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Substract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
